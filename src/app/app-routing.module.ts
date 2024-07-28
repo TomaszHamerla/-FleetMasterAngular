872375338bad8cdb4b174e95e269326881f2a8bf
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-07-28 13:39:09 +0200

    added reoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./views/register/register.component";
+import {LoginComponent} from "./views/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: '', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
