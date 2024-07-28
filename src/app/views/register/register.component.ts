9588034a3a649b82b704448e075a773b798ab62d
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-07-28 13:39:24 +0200

    added register

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  username: any;
+  password: any;
+  passwordVisible: boolean = false;
+  email: any;
 
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  changeToLoginForm() {
+    this.router.navigate(['/login']);
+  }
 }
